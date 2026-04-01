d810447c61c527d0f10e8c940ea8db54cb71be9d
kms <samantinoro@unomaha.edu>
2026-04-01 03:19:44 -0500

    solved q2 and q3

--- sudoku_board.py
+++ sudoku_board.py
@@ -9,6 +9,27 @@ class Sudoku:
     def __str__(self) -> str:
         s = ""
         # YOUR CODE HERE
+
+        #print(sorted(self.sudoku.items()))
+
+        strlist = [ s[1] for s in sorted(self.sudoku.items())]
+
+        #print(strlist)
+
+        for i in range(len(strlist)):
+            if i == 0:
+                pass
+            elif i % 27 == 0:
+                s += "\n\n"
+            elif i % 9 == 0:
+                s += "\n"
+            elif i % 3 == 0:
+                s += " "
+
+            s += str(strlist[i]) + " "
+
+        #print(s)
+
         return s
 
     @classmethod
@@ -21,4 +42,14 @@ class Sudoku:
     def from_model(cls, model: clingo.solving.Model) -> "Sudoku":
         sudoku = {}
         # YOUR CODE HERE
-        return cls(sudoku)
+
+        #print(model.symbols(shown=True))
+
+        #3_way_list = [[arg.number for arg in s.arguments] for s in model.symbols(shown=True)]
+
+        board = { (s.arguments[0].number, s.arguments[1].number): s.arguments[2].number
+        for s in sorted(model.symbols(shown=True))  }
+
+        #print(board)
+
+        return cls(board)
